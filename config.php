f73a5f690a580428cee0902b97519b44ddd7ef1e
Vladislav Teteryatnikov <vladislav.teteryatnikov@gmail.com>
2025-07-28 17:28:12 +0300

    create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'inline');
+    define('CHARSET', 'utf8mb4');
+    define('COLLATE', 'utf8mb4_unicode_ci');
+
